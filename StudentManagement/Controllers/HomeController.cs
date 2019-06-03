68c33359924ce68f332a768aa1be34a0786f1f5f
Shilpa <shilli11417@gmail.com>
2019-06-03 13:12:45 +1000

    Upload Photo file of Student

--- StudentManagement/Controllers/HomeController.cs
+++ StudentManagement/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using StudentManagement.Models;
 using StudentManagement.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,10 +13,13 @@ namespace StudentManagement.Controllers
     public class HomeController : Controller
     {
         private readonly IStudentRepository _studentRepository;
+        private readonly IHostingEnvironment hostingEnvironment;
 
-        public HomeController(IStudentRepository studentRepository)
+        public HomeController(IStudentRepository studentRepository,
+                               IHostingEnvironment hostingEnvironment)
         {
             _studentRepository = studentRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         public ViewResult Index()
@@ -42,11 +47,29 @@ namespace StudentManagement.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Student student)
+        public IActionResult Create(StudentCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Student newStudent = _studentRepository.Add(student);
+                string uniqueFileName = null;
+                if(model.Photo != null)
+                {
+                   string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                   uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+
+                Student newStudent = new Student
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Course = model.Course,
+                    PhotoPath = uniqueFileName
+                
+                };
+                    
+                _studentRepository.Add(newStudent);
                 return RedirectToAction("details", new { id = newStudent.Id });
             }
             return View();
--- StudentManagement/ViewModels/StudentCreateViewModel.cs
+++ StudentManagement/ViewModels/StudentCreateViewModel.cs
@@ -1,26 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using StudentManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace StudentManagement.Models
+namespace StudentManagement.ViewModels
 {
-    public class Student
+    public class StudentCreateViewModel
     {
-        public int Id { get; set; }
-
         [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         [Required]
         public string Name { get; set; }
 
         [Required]
-        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", 
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
             ErrorMessage = "Invalid Email Format")]
-        [Display(Name="Business Email")]
+        [Display(Name = "Business Email")]
         public string Email { get; set; }
 
-        public string PhotoPath { get; set; }
+        public IFormFile Photo { get; set; }
 
         [Required]
         public Course? Course { get; set; }
